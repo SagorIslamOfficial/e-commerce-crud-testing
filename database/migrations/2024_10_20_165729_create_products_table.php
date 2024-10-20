1126e4280aae72cbecd6bd3b27cdb2bb63dada11
Muhammad Alamin Islam <alaminislam1274@gmail.com>
2024-10-20 23:42:33 +0600

    Added Controllers, Models, and Migrations with relationships

---
+++ database/migrations/2024_10_20_165729_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            // Define foreign keys
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('subcategory_id')->constrained()->cascadeOnDelete();
+
+            // Define columns of product
+            $table->string('name');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->integer('old_price');
+            $table->integer('new_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
